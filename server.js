62cf8f35328a3e921d6efd937d47d7d421244b68
Jobin Thomas <jobinthomas364@gmail.com>
2022-11-19 02:32:18 +0530

    initial

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");                                //inbuild module
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) =>{
+    res.render("home");
+})
+
+
+
+
+//set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, '/resources/views')); 
+app.set('view engine', 'ejs');
+
+
+
+app.listen(port, ()=>{
+    console.log(`port ${port} running...`)
+});
