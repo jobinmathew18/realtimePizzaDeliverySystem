78b8d70250bf33dc081fab4b42733aadbf343879
Jobin Thomas <jobinthomas364@gmail.com>
2022-11-20 03:59:58 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -8,14 +8,27 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) =>{
-    res.render("home");
-})
-
 //set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, '/resources/views')); 
 app.set('view engine', 'ejs');
+
+
+app.get('/', (req,res) =>{
+    res.render("home");
+});
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+});
+
+app.get('/login', (req,res)=>{
+    res.render('auth/login');
+});
+
+app.get('/register', (req,res)=>{
+    res.render('auth/register');
+});
 
 
 
